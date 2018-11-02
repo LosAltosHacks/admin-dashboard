6340f3c1e47007992833f40b1f2bd519d094af65
linku <lin.kungmin@gmail.com>
2018-11-02 14:20:41 -0700

    fix background image

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
       <li><img src="assets/images/LAHSSmashLongBorderless.png" alt="Logo" class="logo"></li>
       <li id="drop-down">Social<i></i></li>
     </ul>
-    <img src="assets/SSBUGradient.png" alt="Smash Background" id="background">
+    <img src="assets/images/SSBUGradient.png" alt="Smash Background" id="background">
     <!-- <img src="assets/LAHSSmashIcon.png" alt="LAHS Smash" id="logo">
     We are
     <img src="assets/LAHSSmashLongTextOnly.png" alt="LAHS Smash" id="logo"> -->
