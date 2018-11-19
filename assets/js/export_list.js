71aca430f25cb847f3022e12c4b589d30e7887e2
Leo <mml0816@gmail.com>
2018-11-18 16:11:41 -0800

    modify export option

--- assets/js/export_list.js
+++ assets/js/export_list.js
@@ -1,8 +1,36 @@
-function exportCSV() {
+var clipboard = {
+  data: '',
+  intercept: false,
+  hook: function(e) {
+    console.log('hooked');
+    if (clipboard.intercept) {
+      e.preventDefault();
+      e.clipboardData.setData('text/plain', clipboard.data);
+      console.log('hi');
+      clipboard.intercept = false;
+      clipboard.data = '';
+    }
+  }
+}
+
+document.addEventListener('copy', clipboard.hook);
+
+function exportEmails() {
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      var copyText = "";
+      var response = JSON.parse(this.responseText);
+      response.forEach(function(user) {
+        copyText += user.email + "\n";
+      })
+
+      clipboard.data = copyText;
+      clipboard.intercept = true;
+
+      console.log(clipboard.data);
+      document.execCommand('copy');
       // this.responseText
     }
   }
