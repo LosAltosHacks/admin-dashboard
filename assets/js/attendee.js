e9d50ba2c48cbed4be4c03a0a192a3d43b04f934
Josh Hejna <josh.hejna@gmail.com>
2018-12-11 23:19:23 -0800

    Use templates in js

---
+++ assets/js/attendee.js
@@ -0,0 +1,118 @@
+// "use strict";
+// import { escapeHTML } from "helpers.js";
+
+let AttendeeRenderType = Object.freeze({
+    ATTENDEE_LIST: 1,
+    ACCEPTED_LIST: 2,
+    UNACCEPTED_LIST: 3,
+});
+
+class Attendee {
+  constructor(obj) {
+    this.data = {};
+    this.data.user_id = obj.user_id;
+    this.data.first_name = obj.first_name;
+    this.data.surname = obj.surname;
+    this.data.email = obj.email;
+    this.data.age = obj.age;
+    this.data.school = obj.school;
+    this.data.grade = obj.grade;
+    this.data.student_phone_number = obj.student_phone_number;
+    this.data.guardian_name = obj.guardian_name;
+    this.data.guardian_email = obj.guardian_email;
+    this.data.guardian_phone_number = obj.guardian_phone_number;
+    this.data.gender = obj.gender;
+    this.data.tshirt_size = obj.tshirt_size;
+    this.data.previous_hackathons = obj.previous_hackathons;
+    this.data.github_username = obj.github_username;
+    this.data.linkedin_profile = obj.linkedin_profile;
+    this.data.dietary_restrictions = obj.dietary_restrictions;
+    this.data.signed_waiver = obj.signed_waiver;
+    this.data.acceptance_status = obj.acceptance_status;
+    this.data.email_verified = obj.email_verified;
+    this.data.timestamp = obj.timestamp;
+
+    this.elements = {};
+  }
+
+  render(renderType) {
+    if (!this.elements[renderType]) {
+      this.elements[renderType] = document.createElement("div");
+      this.elements[renderType].classList.add("attendees-row");
+      this.elements[renderType].setAttribute("data-id", this.data.user_id);
+    }
+
+    let procData = {};
+    Object.keys(this.data).forEach(key => {
+      procData[key] = escapeHTML(String(this.data[key]));
+    });
+
+    this.elements[renderType].innerHTML = `
+    <details>
+        <summary title="${procData.first_name + " " + procData.surname + " <" + procData.user_id + ">"}">
+            ${(() => {
+                switch (renderType) {
+                    case AttendeeRenderType.ATTENDEE_LIST:
+                        return "<span class='delete-icon'><span title='Delete Attendee'><img src='/assets/icons/close.svg'></span></span>";
+                    case AttendeeRenderType.ACCEPTED_LIST:
+                        return "<span class='unaccept-icon'><span title='Remove Attendee From Queue'><img src='/assets/icons/user-unaccept.svg'></span></span>";
+                    default:
+                        return "";
+                }
+            })()}
+            <ul>
+                ${renderType == AttendeeRenderType.UNACCEPTED_LIST ?
+                    '<li class="acceptance-checkbox"><input class="accept" type="checkbox"></li>':
+                    `<li>${procData.acceptance_status ? "Yes" : "No"}</li>`
+                }
+                <li>${procData.first_name}</li>
+                <li>${procData.surname}</li>
+                <li>${procData.email}</li>
+                <li>${new Date(
+                  procData.timestamp.replace(" ", "T") + "Z"
+                ).toDateString()}</li>
+            </ul>
+        </summary>
+        <div class="attendees-details">
+            <ul>
+                <li><b>Gender:</b>${procData.gender}</li>
+                <li><b>Age:</b>${procData.age}</li>
+                <li><b>School:</b>${procData.school}</li>
+                <li><b>Grade:</b>${procData.grade}</li>
+                <li><b>Phone Number:</b>${procData.student_phone_number}</li>
+                <li><b>T-Shirt Size:</b>${procData.tshirt_size}</li>
+                <li><b>Previous Hackathons:</b>${
+                  procData.previous_hackathons
+                }</li>
+            </ul>
+            <ul>
+                <li><b>Guardian Name:</b>${procData.guardian_name}</li>
+                <li><b>Guardian Email:</b>${procData.guardian_email}</li>
+                <li><b>Guardian Phone Number:</b>${
+                  procData.guardian_phone_number
+                }</li>
+            </ul>
+            <ul>
+                <li><b>GitHub Username:</b>${procData.github_username}</li>
+                <li><b>LinkedIn Profile:</b>${procData.linkedin_profile}</li>
+                <li><b>Dietary Restrictions:</b>${
+                  procData.dietary_restrictions
+                }</li>
+            </ul>
+            <span class='edit-icon'><span title='Edit Attendee Data'>
+                    <img src='/assets/icons/user-edit.svg'>
+            </span></span>
+            <span class='history-icon'><span title='View Edit History'>
+                <img src='/assets/icons/history.svg'>
+            </span></span>
+        </div>
+    </details>`;
+  }
+
+  getElement(renderType) {
+    if (!this.elements[renderType]) {
+      this.render(renderType);
+    }
+    return this.elements[renderType];
+  }
+}
--- assets/js/get_list.js
+++ assets/js/get_list.js
@@ -1,3 +1,5 @@
+// import { Attendee } from "attendee.js";
+
 async function getSubscribedList() {
   $('#email-list ul').empty();
   let response = await request("GET", "/email_list/v1/subscriptions");
@@ -10,125 +12,28 @@ async function getSubscribedList() {
 
 async function getList() {
   let response = await request("GET", "/registration/v1/list");
-  response.forEach(function(user) {
-    var template = document.getElementById("attendee-template");
-    var attendee = template.content.cloneNode(true);
-    var summary = [user.acceptance_status == "none" ? "No" : user.acceptance_status.charAt(0).toUpperCase() + user.acceptance_status.slice(1), user.first_name, user.surname, user.email, (new Date(user.timestamp.replace(" ", "T") + "Z")).toDateString()];
-
-    summary.forEach(function(data) {
-      var element = document.createElement("li");
-      element.textContent = data;
-      attendee.querySelector("summary ul").appendChild(element)
-    })
-
-    attendee.querySelector("summary").setAttribute("title", user.first_name + " " + user.surname + " <" + user.user_id + ">");
-
-    attendee.querySelector("summary").insertAdjacentHTML('afterbegin',
-      "<span class='delete-icon'><span title='Delete Attendee'><img src='/assets/icons/close.svg'></span></span>"
-    );
-
-    attendee.querySelector(".attendees-row").setAttribute("data-id", user.user_id);
-    attendee.querySelector(".gender").appendChild(document.createTextNode(user.gender));
-    attendee.querySelector(".age").appendChild(document.createTextNode(user.age));
-    attendee.querySelector(".school").appendChild(document.createTextNode(user.school));
-    attendee.querySelector(".grade").appendChild(document.createTextNode(user.grade));
-    attendee.querySelector(".tel").appendChild(document.createTextNode(user.student_phone_number));
-    attendee.querySelector(".t-shirt").appendChild(document.createTextNode(user.tshirt_size));
-    attendee.querySelector(".prev-hack").appendChild(document.createTextNode(user.previous_hackathons));
-    attendee.querySelector(".g-name").appendChild(document.createTextNode(user.guardian_name ? user.guardian_name : ""));
-    attendee.querySelector(".g-email").appendChild(document.createTextNode(user.guardian_email ? user.guardian_email : ""));
-    attendee.querySelector(".g-tel").appendChild(document.createTextNode(user.guardian_phone_number ? user.guardian_phone_number : ""));
-    attendee.querySelector(".github-user").appendChild(document.createTextNode(user.github_username ? user.github_username : ""));
-    attendee.querySelector(".linkedin-prof").appendChild(document.createTextNode(user.linkedin_profile ? user.linkedin_profile : ""));
-    attendee.querySelector(".diet-restrict").appendChild(document.createTextNode(user.dietary_restrictions ? user.dietary_restrictions : ""));
-
-    attendee.querySelector(".attendees-details").insertAdjacentHTML('beforeend',
-      "<span class='edit-icon'><span title='Edit Attendee Data'><img src='/assets/icons/user-edit.svg'></span></span>"
-    )
-
-    attendee.querySelector(".attendees-details").insertAdjacentHTML('beforeend',
-      "<span class='history-icon'><span title='View Edit History'><img src='/assets/icons/history.svg'></span></span>"
-    )
-
-    document.getElementById("attendees-list").insertBefore(attendee, document.getElementById("attendees-list").children[1]);
+  response.forEach(userData => {
+    let attendee = new Attendee(userData);
+    let element = attendee.getElement(AttendeeRenderType.ATTENDEE_LIST);
+    document.getElementById("attendees-list").insertBefore(element, document.getElementById("attendees-list").children[1]);
   })
 }
 
 async function getAcceptedList() {
   let response = await getUser({acceptance_status: "queue"});
-  response.forEach(function(user) {
-    var template = document.getElementById("attendee-template");
-    var attendee = template.content.cloneNode(true);
-    var summary = [user.acceptance_status.charAt(0).toUpperCase() + user.acceptance_status.slice(1), user.first_name, user.surname, user.email, (new Date(user.timestamp.replace(" ", "T") + "Z")).toDateString()];
-
-    summary.forEach(function(data) {
-      var element = document.createElement("li");
-      element.textContent = data;
-      attendee.querySelector("summary ul").appendChild(element)
-    })
-
-    attendee.querySelector("summary").insertAdjacentHTML('afterbegin',
-      "<span class='unaccept-icon'><span title='Remove Attendee From Queue'><img src='/assets/icons/user-unaccept.svg'></span></span>"
-    );
-
-    attendee.querySelector(".attendees-row").setAttribute("data-id", user.user_id);
-    attendee.querySelector(".gender").appendChild(document.createTextNode(user.gender));
-    attendee.querySelector(".age").appendChild(document.createTextNode(user.age));
-    attendee.querySelector(".school").appendChild(document.createTextNode(user.school));
-    attendee.querySelector(".grade").appendChild(document.createTextNode(user.grade));
-    attendee.querySelector(".tel").appendChild(document.createTextNode(user.student_phone_number));
-    attendee.querySelector(".t-shirt").appendChild(document.createTextNode(user.tshirt_size));
-    attendee.querySelector(".prev-hack").appendChild(document.createTextNode(user.previous_hackathons));
-    attendee.querySelector(".g-name").appendChild(document.createTextNode(user.guardian_name ? user.guardian_name : ""));
-    attendee.querySelector(".g-email").appendChild(document.createTextNode(user.guardian_email ? user.guardian_email : ""));
-    attendee.querySelector(".g-tel").appendChild(document.createTextNode(user.guardian_phone_number ? user.guardian_phone_number : ""));
-    attendee.querySelector(".github-user").appendChild(document.createTextNode(user.github_username ? user.github_username : ""));
-    attendee.querySelector(".linkedin-prof").appendChild(document.createTextNode(user.linkedin_profile ? user.linkedin_profile : ""));
-    attendee.querySelector(".diet-restrict").appendChild(document.createTextNode(user.dietary_restrictions ? user.dietary_restrictions : ""));
-
-    document.getElementById("accepted-list").insertBefore(attendee, document.getElementById("accepted-list").children[1]);
+  response.forEach(userData => {
+    let attendee = new Attendee(userData);
+    let element = attendee.getElement(AttendeeRenderType.ACCEPTED_LIST);
+    document.getElementById("accepted-list").insertBefore(element, document.getElementById("accepted-list").children[1]);
   })
 }
 
 async function getUnacceptedList() {
   let response = await getUser({acceptance_status: "none"});
-  response.forEach(function(user) {
-    var template = document.getElementById("attendee-template");
-    var attendee = template.content.cloneNode(true);
-    var summary = [user.first_name, user.surname, user.email, (new Date(user.timestamp.replace(" ", "T") + "Z")).toDateString()];
-
-    var wrapper = document.createElement("li");
-    wrapper.classList.add("acceptance-checkbox");
-    var checkbox = document.createElement("input");
-    checkbox.classList.add("accept");
-    var type = document.createAttribute("type");
-    type.value = "checkbox";
-    checkbox.setAttributeNode(type);
-    wrapper.appendChild(checkbox);
-    attendee.querySelector("summary ul").appendChild(wrapper);
-
-    summary.forEach(function(data) {
-      var element = document.createElement("li");
-      element.textContent = data;
-      attendee.querySelector("summary ul").appendChild(element);
-    })
-
-    attendee.querySelector(".attendees-row").setAttribute("data-id", user.user_id);
-    attendee.querySelector(".gender").appendChild(document.createTextNode(user.gender));
-    attendee.querySelector(".age").appendChild(document.createTextNode(user.age));
-    attendee.querySelector(".school").appendChild(document.createTextNode(user.school));
-    attendee.querySelector(".grade").appendChild(document.createTextNode(user.grade));
-    attendee.querySelector(".tel").appendChild(document.createTextNode(user.student_phone_number));
-    attendee.querySelector(".t-shirt").appendChild(document.createTextNode(user.tshirt_size));
-    attendee.querySelector(".prev-hack").appendChild(document.createTextNode(user.previous_hackathons));
-    attendee.querySelector(".g-name").appendChild(document.createTextNode(user.guardian_name ? user.guardian_name : ""));
-    attendee.querySelector(".g-email").appendChild(document.createTextNode(user.guardian_email ? user.guardian_email : ""));
-    attendee.querySelector(".g-tel").appendChild(document.createTextNode(user.guardian_phone_number ? user.guardian_phone_number : ""));
-    attendee.querySelector(".github-user").appendChild(document.createTextNode(user.github_username ? user.github_username : ""));
-    attendee.querySelector(".linkedin-prof").appendChild(document.createTextNode(user.linkedin_profile ? user.linkedin_profile : ""));
-    attendee.querySelector(".diet-restrict").appendChild(document.createTextNode(user.dietary_restrictions ? user.dietary_restrictions : ""));
-
-    document.getElementById("unaccepted-list").insertBefore(attendee, document.getElementById("unaccepted-list").children[1]);
+  response.forEach(userData => {
+    let attendee = new Attendee(userData);
+    let element = attendee.getElement(AttendeeRenderType.UNACCEPTED_LIST);
+    document.getElementById("unaccepted-list").insertBefore(element, document.getElementById("unaccepted-list").children[1]);
   })
 }
 
