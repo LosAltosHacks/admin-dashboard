ef5104732ef0f30cb3a3c84e85f64bbc966ef491
Leo <mml0816@gmail.com>
2018-12-28 15:17:35 -0800

    change to production

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,10 @@
-// server = "https://api.losaltoshacks.com";
-server = "http://localhost:5000";
+server = "https://api.losaltoshacks.com";
 
 // Listeners for controls
-// let jwt_auth = localStorage.jwt_auth;
-// if (window.location.pathname !== "/login.html" && !localStorage.jwt_auth) {
-//   window.location.href = "/login.html";
-// }
-let jwt_auth = "foobar";
+let jwt_auth = localStorage.jwt_auth;
+if (window.location.pathname !== "/login.html" && !localStorage.jwt_auth) {
+  window.location.href = "/login.html";
+}
 
 let edited_fields = {};
 
@@ -15,8 +13,8 @@ $(document).ready(function() {
   if (localStorage.panel) getPanel(localStorage.panel);
 
   // Decorative Controls
-  // $("#profile > #profile-pic").css({'background-image': "url('" + localStorage.prof_image + "')"});
-  // $("#profile > span").not("#profile-pic").append("<h4>" + localStorage.name.split(' ')[0] + " " + localStorage.name.split(' ')[1].charAt(0) + ".</h4>");
+  $("#profile > #profile-pic").css({'background-image': "url('" + localStorage.prof_image + "')"});
+  $("#profile > span").not("#profile-pic").append("<h4>" + localStorage.name.split(' ')[0] + " " + localStorage.name.split(' ')[1].charAt(0) + ".</h4>");
 
   $("#profile > #profile-pic").hover(function() {
     $(this).css({'background-image': "url('/assets/icons/sign-out.svg')"});
